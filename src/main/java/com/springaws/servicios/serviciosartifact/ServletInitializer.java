7e802b8f9fc2f27d47db82917188e90075404dba
Luis Pimentel <pimenteles1@gmail.com>
2019-02-07 12:21:01 -0600

    Configuración web
    Se agrego la configuración para thymeleaf y una pagina con el framework materialize

---
+++ src/main/java/com/springaws/servicios/serviciosartifact/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.springaws.servicios.serviciosartifact.config;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServiciosApplication.class);
+	}
+
+}
--- src/main/java/com/springaws/servicios/serviciosartifact/mvc/view/HomeController.java
+++ src/main/java/com/springaws/servicios/serviciosartifact/mvc/view/HomeController.java
@@ -1,39 +1,16 @@
 package com.springaws.servicios.serviciosartifact.mvc.view;
 
-import com.springaws.servicios.serviciosartifact.mvc.model.dao.PersistenceExampleDao;
-import com.springaws.servicios.serviciosartifact.mvc.model.persistence.PersistenceExample;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Created by IDEX1010 on 30/01/2019.
- */
 
 @Controller
 public class HomeController {
 
-    @Value("${spring.application.name}")
-    private String appName;
-
-
-    @Autowired
-    private PersistenceExampleDao persistenceExampleDao;
-
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String homePage(Model model, @RequestParam(required = false) String rfc){
-
-        model.addAttribute("appName", appName);
-
-        persistenceExampleDao.findByRfc(rfc);
-
+    public String homePage(){
         return "home";
-
     }
 
 }
