7f164f01ea430c049cdc2c2e7dba5be5bbf4b647
Cesar <cesarcruzcll@hotmail.com>
2019-03-24 20:41:56 -0600

    Se agrega Cat Tipo de Divisas

---
+++ src/main/java/com/springaws/servicios/serviciosartifact/mvc/model/repository/CatTipoDivisaRepository.java
@@ -0,0 +1,7 @@
+package com.springaws.servicios.serviciosartifact.mvc.model.repository;
+
+import com.springaws.servicios.serviciosartifact.mvc.model.persistence.CatTipoDivisa;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatTipoDivisaRepository extends CrudRepository<CatTipoDivisa,Long> {
+}
